efaf109d1663dfddf120bff850253a0bf9317370
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2022-01-05 23:48:33 +0300

    add AppVeyor

--- src/test/java/ru/netology/rest/MobileBankApiTest.java
+++ src/test/java/ru/netology/rest/MobileBankApiTest.java
@@ -3,6 +3,7 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class MobileBankApiTest {
 
@@ -17,6 +18,7 @@ public class MobileBankApiTest {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
